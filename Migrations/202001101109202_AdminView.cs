a8224eca9e7e138c28887f313461605815b7b960
apalasopoulos1986 <a_palasopoulos@yahoo.com>
2020-01-10 13:10:03 +0200

    MigrateViews

---
+++ Migrations/202001101109202_AdminView.cs
@@ -0,0 +1,16 @@
+namespace ChatAppTest1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AdminView : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
